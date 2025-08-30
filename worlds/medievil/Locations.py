1b77a5c3d8eb34be942a53286115d930e6800bb0
riezahughes <dan@tantalus-troupe.com>
2025-08-30 22:48:16 +0200

    Hall of heroes book and gargoyle

--- worlds/medievil/Locations.py
+++ worlds/medievil/Locations.py
@@ -122,7 +122,9 @@ location_tables = {
         MedievilLocationData("Chalice Reward 17","Gold Shield Ammo (100)",MedievilLocationCategory.CHALICE_REWARD),
         MedievilLocationData("Chalice Reward 18","Gold Shield Ammo (100)",MedievilLocationCategory.CHALICE_REWARD),
         MedievilLocationData("Chalice Reward 19","Gold Shield Ammo (100)",MedievilLocationCategory.CHALICE_REWARD),
-        MedievilLocationData("Chalice Reward 20","Gold Shield Ammo (100)",MedievilLocationCategory.CHALICE_REWARD)
+        MedievilLocationData("Chalice Reward 20","Gold Shield Ammo (100)",MedievilLocationCategory.CHALICE_REWARD),
+        MedievilLocationData("Book: Heroes of Eld - HH","Gold Shield Ammo (100)",MedievilLocationCategory.BOOK),
+        MedievilLocationData("Gargoyle: Entrance - HH","Gold Shield Ammo (100)",MedievilLocationCategory.GARGOYLE),
     ],
 
     "Dan's Crypt": [
